26ddf11d730c9e276736fe717edda620c133e447
Kerry <trankerry94@gmail.com>
2021-06-17 22:35:48 -0700

    added a bit more

--- node-http/server.js
+++ node-http/server.js
@@ -3,4 +3,8 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer(req, res)
+const server = http.createServer((req, res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type')
+});
